bebcd89b42d849a17893bc5ca5aa93043e7bb353
Xun Jian <nafi.professional@outlook.com>
2025-02-19 17:24:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World 1.0 with automate trigger'
+    return 'Welcome to Python Flask World 1.0 with automate trigger BUILD NUMBER: 1'
 
 
 if __name__ == '__main__':
